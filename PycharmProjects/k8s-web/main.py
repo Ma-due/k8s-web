9f37c0fc04f2e8a1db429885cb3d2fc4127777c6
Ma-due <maduee2@naver.com>
2022-08-11 02:14:05 +0900

    [feat] 초기 프로젝트 파일

---
+++ PycharmProjects/k8s-web/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+VERSION = "V1"
+
+
+@app.route('/')
+def home():
+    return socket.gethostname()
+
+
+@app.route('/name')
+def hostName():
+    return socket.gethostname()
+
+
+@app.route('/version')
+def ip():
+    return VERSION
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
