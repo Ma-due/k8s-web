1bc1414cd4fbf30e347fc325f0474a7b7787659c
Ma-due <root@master.us-west1-b.c.rock-figure-370701.internal>
2023-01-03 03:19:13 +0000

    delete key

--- k8s-install.sh
+++ k8s-install.sh
@@ -27,6 +27,13 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 }
 EOF
 
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
+apt-get update
+apt-get install helm
+
 systemctl enable docker
 systemctl daemon-reload
 systemctl restart docker
